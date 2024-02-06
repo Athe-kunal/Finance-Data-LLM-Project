f390f71f3d8251a1bb8b6de93e88a1fe04b05581
Athe-kunal <athekunal@gmail.com>
2024-02-06 12:06:22 -0500

    fastapi server

--- src/server.py
+++ src/server.py
@@ -4,9 +4,30 @@
 Give an API request to all the backend requests
 """
 
+from functools import lru_cache
 from fastapi import FastAPI
 from pydantic import BaseModel
 from src.vectorDatabase import create_database
 from src.queryDatabase import query_database_earnings_call, query_database_sec
+from .settings import QdrantSettings
 
 app = FastAPI()
+
+@lru_cache
+def get_settings():
+    return QdrantSettings()
+
+@app.get("data/{ticker}/{year}")
+async def ticker_year(ticker:str,year:int):
+    (
+        qdrant_client,
+        encoder,
+        speakers_list_1,
+        speakers_list_2,
+        speakers_list_3,
+        sec_form_names,
+        earnings_call_quarter_vals,
+    ) = create_database(ticker=ticker, year=year)
+
+# @app.get("Earnings/{question}/{quarter}")
+# async def earnings_chat(question:str,quarter:str):
---
+++ src/settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+from qdrant_client import QdrantClient
+from sentence_transformers import SentenceTransformer
+from typing import List
+
+class QdrantSettings(BaseSettings):
+    qdrant_client: QdrantClient 
+    encoder: SentenceTransformer
+    speaker_list_1: List[str]
+    speaker_list_2: List[str]
+    speaker_list_3: List[str]
+    sec_form_names: List[str]
+    earnings_call_quarter_vals: List[str]
+
